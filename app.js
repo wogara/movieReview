0433d6e7ef7d7e7c9e3c699d3d6683d028c464ff
William O'Gara <39812727+wogara@users.noreply.github.com>
2020-08-25 16:25:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+var express    = require('express'),
+	app        = express(),
+	bodyParser = require('body-parser'),
+	mongoose   = require('mongoose'),
+	passport   = require("passport"),
+	Movie = require('./models/movie'),
+	Comment    = require("./models/comment"),
+	seedDB     = require("./seeds"),
+	User       = require("./models/user"),
+	LocalStrategy = require("passport-local");
+	methodOverride = require("method-override");
+	flash      = require('connect-flash');
+
+var commentRoutes = require("./routes/comments"),
+	movieRoutes = require("./routes/movies");
+	indexRoutes = require("./routes/index");
+
+
+
+//seedDB();
+var url = process.env.DATABASEURL || "mongodb+srv://wogara:Quaresma7!@cluster0-tsjiz.mongodb.net/test?retryWrites=true&w=majority";
+//mongoose.connect("mongodb://localhost/yelp_camp",{useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });//{ useNewUrlParser: true });
+//mongoose.connect("mongodb+srv://wogara:Quaresma7!@cluster0-tsjiz.mongodb.net/test?retryWrites=true&w=majority",{
+mongoose.connect(url,{
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("connected to db");
+}).catch(err =>{
+	console.log("ERROR: ",err.message);
+});
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static(__dirname+ "/public"));
+app.use(flash());
+
+app.use(require("express-session")({
+	secret: "bruno is a very good boy named ezekiel",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+})
+
+app.use(indexRoutes);
+app.use("/movies",movieRoutes);
+app.use("/movies/:id/comments",commentRoutes);
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("app listening on port ");
+});
